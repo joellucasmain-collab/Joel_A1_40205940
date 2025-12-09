44561147d6692bb09a2ff568d205cb7a5775a64c
40205940 <40205940@HY-R104-10.resource.uc>
2025-12-09 12:42:02 +0000

    Assignment 1 first commit

---
+++ src/main/java/com/mycompany/joela140205940/JoelA140205940.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.joela140205940;
+
+/**
+ *
+ * @author 40205940
+ */
+public class JoelA140205940 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
